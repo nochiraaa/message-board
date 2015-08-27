768b48f2749479090084264d205fae8509a3558f
Yuki Aihara <aiharayuki0702@gmail.com>
2015-08-27 06:25:56 +0000

    impkementation age

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20}, presence: true
     
     validates :body, length: {minimum: 2, maximum: 30}, presence: true
+    
+    validates :age, numericality: { only_interger: true, :greater_than_or_equal_to: 0, }, presence: true
 end
