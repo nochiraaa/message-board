a6c242e0d613d51125d3c5dca63c78ff3e1f9c76
Yuki Aihara <aiharayuki0702@gmail.com>
2015-08-27 07:49:03 +0000

    activerecord pendingmigrationerror

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825131716) do
+ActiveRecord::Schema.define(version: 20150827061018) do
 
-  create_table "messages", force: :cascade do |t|r
+  create_table "messages", force: :cascade do |t|
     t.string   "name"
-    t.string   "age"
+    #t.string   "age"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
